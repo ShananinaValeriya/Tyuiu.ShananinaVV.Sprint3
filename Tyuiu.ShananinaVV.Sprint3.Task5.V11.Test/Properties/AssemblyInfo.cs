448c7da99da4de524ad651a56c5edd75f058d32a
Tyuiu.ShananinaVV@outlook.com <valeriya.v.2508@gmail.com>
2023-10-28 16:18:53 +0500

    Task5

---
+++ Tyuiu.ShananinaVV.Sprint3.Task5.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShananinaVV.Sprint3.Task5.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShananinaVV.Sprint3.Task5.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f035855f-ace8-4615-a286-319f91319b23")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShananinaVV.Sprint3.Task5.V11/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShananinaVV.Sprint3.Task5.V11.Lib;
+
+namespace Tyuiu.ShananinaVV.Sprint3.Task5.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Шананина В. В. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнила: Шананина Валерия Викторовна | ИСТНб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет сумму сумм ряда по формуле,       *");
+            Console.WriteLine("* при x = 5.                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+
+            Console.WriteLine("Переменная X = " + x);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Сумма сумм ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadKey();
+        }
+    }
+}
