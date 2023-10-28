362fae5306edbca91e30c17a37a1c09285083d4d
Tyuiu.ShananinaVV@outlook.com <valeriya.v.2508@gmail.com>
2023-10-28 15:47:53 +0500

    Task2

---
+++ Tyuiu.ShananinaVV.Sprint3.Task2.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShananinaVV.Sprint3.Task2.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShananinaVV.Sprint3.Task2.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70ad2a8e-72df-4e1d-b8ec-62c34b3c50b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
